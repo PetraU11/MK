8efebecee6cca2b70fe433b134b31fcfb37da9f2
Petra Ulicna <petra.ulicna@hotmail.com>
2023-04-18 13:41:47 +0200

    header icons clickable - js added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const email = document.querySelector("#email-icon")
+/* const email = document.querySelector("#email-icon")
 const newEmailItem = document.createElement("p")
 
 email.addEventListener("click", () => {
@@ -11,7 +11,7 @@ newEmailItem.classList.add("jsEmailShown")
 
 
 
-
+//přidat classes only?
 
 
 
@@ -37,4 +37,38 @@ const phoneBack = document.createElement("p")
 newPhoneItem.addEventListener("click", () => {
     phoneBack.innerHTML =`<img class="header__icons-phone" id="phone-icon" src="images/phone_bw_32.png" alt="ikona telefonu">` 
     newPhoneItem.parentNode.replaceChild(phoneBack,newPhoneItem)
+})
+
+
+*/
+
+const email = document.querySelector("#email-icon")
+const emailShown = document.querySelector("#email-address")
+
+email.addEventListener("click", () => {
+    email.classList.toggle("header__icons-emailIconHidden")
+    emailShown.classList.remove("header__icons-emailAddressHidden")
+    emailShown.classList.add("header__icons-emailAddressShown")
+})
+
+emailShown.addEventListener("click",() => {
+    emailShown.classList.remove("header__icons-emailAddressShown")
+    emailShown.classList.add("header__icons-emailAddressHidden")
+    email.classList.toggle("header__icons-emailIconHidden")
+})
+
+
+const phone = document.querySelector("#phone-icon")
+const phoneShown = document.querySelector("#phone-number") 
+
+phone.addEventListener("click", () => {
+    phone.classList.toggle("header__icons-phoneIconHidden")
+    phoneShown.classList.remove("header__icons-phoneNumberHidden")
+    phoneShown.classList.add("header__icons-phoneNumberShown")
+})
+
+phoneShown.addEventListener("click",() => {
+    phoneShown.classList.remove("header__icons-phoneNumberShown")
+    phoneShown.classList.add("header__icons-phoneNumberHidden")
+    phone.classList.toggle("header__icons-phoneIconHidden")
 })
