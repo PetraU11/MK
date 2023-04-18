a3085001cae5b276fddf69e85a9334f924c6abd8
Petra Ulicna <petra.ulicna@hotmail.com>
2023-04-18 22:43:20 +0200

    header done

--- js/script.js
+++ js/script.js
@@ -46,29 +46,32 @@ const email = document.querySelector("#email-icon")
 const emailShown = document.querySelector("#email-address")
 
 email.addEventListener("click", () => {
-    email.classList.toggle("header__icons-emailIconHidden")
-    emailShown.classList.remove("header__icons-emailAddressHidden")
-    emailShown.classList.add("header__icons-emailAddressShown")
+   // email.classList.toggle("header__icons-emailIconHidden")
+    emailShown.classList.toggle("header__icons-emailAddressHidden")
+    emailShown.classList.toggle("header__icons-emailAddressShown")
+    
 })
 
 emailShown.addEventListener("click",() => {
     emailShown.classList.remove("header__icons-emailAddressShown")
     emailShown.classList.add("header__icons-emailAddressHidden")
-    email.classList.toggle("header__icons-emailIconHidden")
+    // email.classList.toggle("header__icons-emailIconHidden")
 })
+
+
 
 
 const phone = document.querySelector("#phone-icon")
 const phoneShown = document.querySelector("#phone-number") 
 
 phone.addEventListener("click", () => {
-    phone.classList.toggle("header__icons-phoneIconHidden")
-    phoneShown.classList.remove("header__icons-phoneNumberHidden")
-    phoneShown.classList.add("header__icons-phoneNumberShown")
+    // phone.classList.toggle("header__icons-phoneIconHidden")
+    phoneShown.classList.toggle("header__icons-phoneNumberHidden")
+    phoneShown.classList.toggle("header__icons-phoneNumberShown")
 })
 
 phoneShown.addEventListener("click",() => {
     phoneShown.classList.remove("header__icons-phoneNumberShown")
     phoneShown.classList.add("header__icons-phoneNumberHidden")
-    phone.classList.toggle("header__icons-phoneIconHidden")
+    // phone.classList.toggle("header__icons-phoneIconHidden")
 })
