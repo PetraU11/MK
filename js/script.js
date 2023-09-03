4af95b15252c5eccf022221b19ba07862d29a48b
Petra Ulicna <petra.ulicna@hotmail.com>
2023-09-03 21:24:06 +0200

    final ?

--- js/script.js
+++ js/script.js
@@ -34,43 +34,6 @@ removeClass()
 
 
 
-
-
-const email = document.querySelector("#email-icon")
-const emailShown = document.querySelector("#email-address")
-
-email.addEventListener("click", () => {
-   // email.classList.toggle("header__icons-emailIconHidden")
-    emailShown.classList.toggle("header__icons-emailAddressHidden")
-    emailShown.classList.toggle("header__icons-emailAddressShown")
-    
-})
-
-emailShown.addEventListener("click",() => {
-    emailShown.classList.remove("header__icons-emailAddressShown")
-    emailShown.classList.add("header__icons-emailAddressHidden")
-    // email.classList.toggle("header__icons-emailIconHidden")
-})
-
-
-const phone = document.querySelector("#phone-icon")
-const phoneShown = document.querySelector("#phone-number") 
-
-phone.addEventListener("click", () => {
-    // phone.classList.toggle("header__icons-phoneIconHidden")
-    phoneShown.classList.toggle("header__icons-phoneNumberHidden")
-    phoneShown.classList.toggle("header__icons-phoneNumberShown")
-    
-})
-
-phoneShown.addEventListener("click",() => {
-    phoneShown.classList.remove("header__icons-phoneNumberShown")
-    phoneShown.classList.add("header__icons-phoneNumberHidden")
-    // phone.classList.toggle("header__icons-phoneIconHidden")
-})
-
-
-
 const showButton = document.getElementById("showList")
 const hideButton = document.getElementById("hideList")
 const listPartTwo = document.getElementById("listSecondPart")
