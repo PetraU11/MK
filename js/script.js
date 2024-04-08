93fc23d24969d1ddf7bfcd549fd2b57a5c858b03
Petra Ulicna <petra.ulicna@hotmail.com>
2024-04-08 10:57:17 +0200

    test form fiels on ios

--- js/script.js
+++ js/script.js
@@ -46,7 +46,6 @@ const listPartTwo = document.getElementById("listSecondPart")
 
 showButton.addEventListener("click", () => {
 listPartTwo.style.display = "inline-block"
-listPartTwo.style.marginTop = "-16px"
 showButton.style.display = "none"
 hideButton.style.display = "inline-block"
 })
@@ -61,70 +60,3 @@ hideButton.addEventListener("click", () => {
 
 
 
-// REREFENCE - ChatGPT
-
-/*
-    // Načtení uložených komentářů při načtení stránky
-    loadFromLocalStorage();
-  
-    // Přidejte posluchače událostí pro odeslání formuláře
-    const form = document.querySelector(".reference__form");
-    form.addEventListener("submit", function(event) {
-      event.preventDefault();
-  
-      // Získejte hodnoty vstupních polí
-      const name = document.getElementById("jmeno").value;
-      const date = document.getElementById("datum").value;
-      const experience = document.getElementById("info").value;
-  
-      // Uložte komentář do localStorage
-      saveToLocalStorage(name, date, experience);
-  
-      // Vyčištění formuláře
-      form.reset();
-  
-      // Zobrazte potvrzení odeslání
-      const submitDone = document.querySelector(".form_submit-done");
-      submitDone.style.display = "block";
-  
-      // Aktualizace zobrazení komentářů
-      loadFromLocalStorage();
-    });
-  
-  
-  function saveToLocalStorage(name, date, experience) {
-    if (typeof(Storage) !== "undefined") {
-      let comments = JSON.parse(localStorage.getItem("commentData")) || {};
-      const id = Date.now().toString(); // Unikátní identifikátor pro každý komentář
-      comments[id] = { name, date, experience };
-      localStorage.setItem("commentData", JSON.stringify(comments));
-    } else {
-      console.error("LocalStorage není k dispozici.");
-    }
-  }
-  
-  function loadFromLocalStorage() {
-    if (typeof(Storage) !== "undefined") {
-      const commentDiv = document.querySelector(".reference__comment");
-      const storedData = localStorage.getItem("commentData");
-      if (storedData) {
-        const comments = JSON.parse(storedData);
-        let commentsHTML = "";
-        Object.values(comments).forEach(comment => {
-          commentsHTML += `
-            <div class="comment"> 
-              <p>Jméno: ${comment.name} || Datum: ${comment.date}</p>
-              <p class="comment__feedback">" ${comment.experience} "</p>
-            </div>
-          `;
-        });
-        commentDiv.innerHTML = commentsHTML;
-      }
-    } else {
-      console.error("LocalStorage není k dispozici.");
-    }
-  }
-  
- // localStorage.clear()
-
- */
